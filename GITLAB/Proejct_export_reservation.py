a62935512f55072178ac03c59764f4bcb859a9b2
Douggy91 <ejrwls91@gmail.com>
2023-07-15 15:35:00 +0000

    update import time sleep

--- GITLAB/Proejct_export_reservation.py
+++ GITLAB/Proejct_export_reservation.py
@@ -22,7 +22,7 @@ def reservation_export(project_list):
     url=target_url+'/'+str(project_id)+'/export'
     msg=requests.post(target_url+'/'+str(project_id)+'/export', headers={'PRIVATE-TOKEN':gg_token}, verify=False)
     print(f"{msg} : {url}")
-
+    time.sleep(10)
     reservation_export(project_list[1:])
 
 
@@ -36,12 +36,18 @@ def get_download_export(project_list):
     project_id = project_list[0]
     url=target_url+'/'+str(project_id)+'/export'
     status=requests.get(url, headers={'PRIVATE-TOKEN':gg_token}, verify=False)
-    print(f"{status.ok} / {url}")
-    if status.ok:
+    time.sleep(20)
+    if status.ok: 
         content=requests.get(url+'/download', headers={'PRIVATE-TOKEN':gg_token}, verify=False)
-        open(path+'/'+re.sub("/","@",pj_dict[project_id])+'.tar.gz','wb').write(content.content)
+        time.sleep(20)
+        result=open(path+'/'+re.sub("/","@",pj_dict[project_id])+'.tar.gz','wb').write(content.content)
+        time.sleep(20)
 #        open(path+'/'+str(project_id)+"@"+re.sub("/","@",pj_dict[project_id])+'_export.tar.gz','wb').write(content.content)
-        time.sleep(2)
+        print(f"""
+{result}
+{status}
+{content}
+""")
         
     get_download_export(project_list[1:])
 
@@ -59,10 +65,10 @@ def import_project_file(file_list):
     data = {
         "path": (file_name.split('@')[-1]).split('.')[0],
         "namespace": namespace.rstrip("/"),
-        "overwrite" : True
+        "overwrite": "true"
         }
-    result=requests.post(url, headers={'PRIVATE-TOKEN':import_token}, verify=False ,data=data, files=files)
-    time.sleep(2)
+    result=requests.post(url, headers={'PRIVATE-TOKEN':import_token} ,data=data, files=files, verify=False)
+    time.sleep(10)
     print(f"""
 {file_name}
 {file_path}
@@ -73,6 +79,6 @@ def import_project_file(file_list):
 """)
     import_project_file(file_list[1:])
 
-reservation_export(list(pj_dict.keys()))
-get_download_export(list(pj_dict.keys()))
+#reservation_export(list(pj_dict.keys()))
+#get_download_export(list(pj_dict.keys()))
 import_project_file(os.listdir(path))
